227e14b9652757b60e3f00f71939df0c9044df39
Kane Dylan <cool.kdtech1234@gmail.com>
2025-05-24 11:50:21 +0530

    new pattern added

---
+++ basic/Pattern10.java
@@ -0,0 +1,35 @@
+package basic;
+
+public class Pattern10 {
+    public static void main(String[] args){
+        int a=5;
+
+        for(int i=0; i<a; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" *");
+            }
+            int s= 2*(a-i);
+            for(int j=0; j<s; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print(" *");
+            }
+            System.out.println("  ");
+        }
+
+        for(int i=a; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print(" *");
+            }
+            int s= 2*(a-i);
+            for(int j=0; j<s; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print(" *");
+            }
+            System.out.println("  ");
+        }
+    }
+}
--- basic/Pattern9.java
+++ basic/Pattern9.java
@@ -2,13 +2,18 @@ package basic;
 
 public class Pattern9 {
   public static void main(String[] args) {
-    int a = 4;
+    int a =5;
 
     for (int i = 1; i <= a; i++) {
       for (int j = 1; j <= i; j++) {
-        System.out.print(j);
+        int n = i + j;
+        if (n % 2 == 0) {
+          System.out.print(" 1");
+        } else {
+          System.out.print(" 0");
+        }
       }
-      System.out.println(" ");
+        System.out.println(" ");
     }
   }
 }
