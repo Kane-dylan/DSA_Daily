b7fae713850858a85d36adeb275bd588a5bd8348
Kane Dylan <cool.kdtech1234@gmail.com>
2025-05-27 01:34:44 +0530

    functions

--- JAVA/Exercise/prob4.java
+++ JAVA/Exercise/prob4.java
@@ -1,4 +1,17 @@
 package JAVA.Exercise;
 
+import java.util.Scanner;
+
 public class prob4 {
+    public static float Circumference(int r){
+        float circumference = (float) (2 * Math.PI * r);
+        return circumference;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of a circle: ");
+        float a = sc.nextFloat();
+
+    System.out.println(Circumference((int) a));
+    }
 }
